a7bc801be38b8984133c70f4dd9ea752143b20f9
kelson-cosme <kelson.almeida123@gmail.com>
2023-05-08 11:30:36 -0400

    teste

--- src/components/Pages/Home/index.jsx
+++ src/components/Pages/Home/index.jsx
@@ -4,47 +4,48 @@ import axios from "axios";
 import React, {useState, useEffect} from "react";
 
 import "./style.css"
+import { number, object } from "yup";
 
 
 export default function Home(){
 
-    const [posts, setPosts ] = useState([])
+    let [posts, setPosts ] = useState([]);
 
     useEffect(() => { //redenrizar após atualizar o post
         axios.get("http://localhost:5500/api")
-        .then((response) => { //reponse.data para pegar os arrays da api
-            setPosts(response.data) //guardando o registro da api
-            // console.log(response.data.users[0])
+        .then(response => { //reponse.data para pegar os arrays da api
+            setPosts(response.data.users) //guardando o registro da api
+            // console.log(response.data.users)
         })
         .catch(() => {
             console.log("deu errado")
         })
+
     }, [])
 
-
+   
     return(
 
         <div>
-            <Navbar/>
 
+            <Navbar/>
 
             <div className="home-corpo">
 
-                {posts && posts.map((post, key) => {
-                    return(
-                        <div className="home-card" key={key}>
-
-                        <Link className="home-link" to={"/vermais"}>
-                            <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=-1&s=1" alt="" />
-                            
-                            <h2>{post.name}</h2>
-
-                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias eaque sapiente, sint repellat earum laudantium unde esse tempore, quas amet qui maiores ipsum delectus, aliquam a aperiam consectetur! Ullam, officiis.</p>
-                        </Link>
-
-                        </div> 
-                    )
-                })}
+                
+                        return(
+                            <div className="home-card" >
+    
+                            <Link className="home-link" to={"/vermais"}>
+                                <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=-1&s=1" alt="" />
+                                
+                                <h2>daddad</h2>
+    
+                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias eaque sapiente, sint repellat earum laudantium unde esse tempore, quas amet qui maiores ipsum delectus, aliquam a aperiam consectetur! Ullam, officiis.</p>
+                            </Link>
+    
+                            </div> 
+                        )
             </div>
         </div>
     );

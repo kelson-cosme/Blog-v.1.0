9b55e260746fcc4b44799af790e604112358fca6
kelson-cosme <kelson.almeida123@gmail.com>
2023-05-08 11:51:10 -0400

    post na api

--- src/components/Pages/Home/index.jsx
+++ src/components/Pages/Home/index.jsx
@@ -4,48 +4,65 @@ import axios from "axios";
 import React, {useState, useEffect} from "react";
 
 import "./style.css"
-import { number, object } from "yup";
+// import { number, object } from "yup";
+// import { get, set } from "react-hook-form";
 
 
 export default function Home(){
 
-    let [posts, setPosts ] = useState([]);
+    // let [posts, setPosts ] = useState([]);
 
-    useEffect(() => { //redenrizar após atualizar o post
-        axios.get("http://localhost:5500/api")
-        .then(response => { //reponse.data para pegar os arrays da api
-            setPosts(response.data.users) //guardando o registro da api
-            // console.log(response.data.users)
-        })
-        .catch(() => {
-            console.log("deu errado")
-        })
+    // useEffect(() => { //redenrizar após atualizar o post
+    //     axios.get("http://localhost:5500/api")
+    //     .then(response => { //reponse.data para pegar os arrays da api
+    //         setPosts(response.data.users) //guardando o registro da api
+    //         // console.log(response.data.users)
+    //     })
+    //     .catch(() => {
+    //         console.log("deu errado")
+    //     })
+    // }, [])
 
-    }, [])
+    const [posts, setPosts] = useState([])
+    const [loading, setLoading] = useState(false)
 
-   
+    useEffect (() => {
+        getPosts();
+    })
+
+    const getPosts = async () => {
+        try{
+            const res = await axios.get("http://localhost:5500/api")
+            setPosts(res.data.users)
+            setLoading(true)
+        } catch (err){
+            alert(err.message);
+        }
+    }
+
     return(
-
         <div>
 
             <Navbar/>
 
             <div className="home-corpo">
 
-                
-                        return(
-                            <div className="home-card" >
-    
-                            <Link className="home-link" to={"/vermais"}>
-                                <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=-1&s=1" alt="" />
-                                
-                                <h2>daddad</h2>
-    
-                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias eaque sapiente, sint repellat earum laudantium unde esse tempore, quas amet qui maiores ipsum delectus, aliquam a aperiam consectetur! Ullam, officiis.</p>
-                            </Link>
-    
-                            </div> 
-                        )
+                    {loading && 
+                        posts.map((post) =>(
+                                <div className="home-card" >
+        
+                                <Link className="home-link" to={"/vermais"}>
+                                    <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=-1&s=1" alt="" />
+                                    
+                                    <h2>{post.name}</h2>
+        
+                                    <p> {post.avatar}</p>
+                                </Link>
+        
+                                </div> 
+                        ))
+                    }
+                        
             </div>
         </div>
     );
